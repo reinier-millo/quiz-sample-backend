b2490f91238ff0c969bfbc3e2ebde61a484b8dcd
Reinier Millo Sánchez <reinier.millo88@gmail.com>
2022-04-07 23:59:29 -0500

    Adding base entry point for service
    Signed-off-by: Reinier Millo Sánchez <reinier.millo88@gmail.com>

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+/**
+ * Copyright (C) 2022
+ * Author: Reinier Millo Sánchez <reinier.millo88@gmail.com>
+ *
+ * This file is part of the Quiz Backend.
+ * This code is licensed under the MIT License.
+ */
+import "module-alias/register";
+import "dotenv/config";
+import { ClusterServer, Logger } from "@ecualead/server";
+
+/* API data routes */
+import AccountRouter from "@/routers/v1/account";
+import QuizRouter from "@/routers/v1/quiz";
+import QuestionRouter from "@/routers/v1/question";
+
+Logger.setLogLevel(process.env.LOG);
+const logger = new Logger("QuizService");
+
+/* Initialize cluster server */
+const clusterServer = ClusterServer.setup();
+
+/* Run cluster with base routes */
+clusterServer.run({
+  "/v1/account": AccountRouter,
+  "/v1/quiz": QuizRouter,
+  "/v1/question": QuestionRouter,
+});
